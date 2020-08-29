4e2589466d3fce70e60123bd3873d006c700839f
michael.schori@talent-factory.ch <michael.schori@talent-factory.ch>
2020-08-29 10:32:48 +0200

    Added single-view for authors

--- book_manager/views.py
+++ book_manager/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Author, Book
 from .forms import AuthorForm, BookForm
 from django.contrib import messages
 
 
-def show_authors(request):
+def list_authors(request):
     authors = Author.objects.all()
     form = AuthorForm()
     if request.method == 'POST':
@@ -12,10 +12,19 @@ def show_authors(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Author successfully created.')
-    return render(request, 'book_manager/show_authors.html', {'authors': authors, 'form': form})
+    return render(request, 'book_manager/list_authors.html', {'authors': authors, 'form': form})
 
 
-def show_books(request):
+def list_books(request):
     books = Book.objects.all()
     form = BookForm()
-    return render(request, 'book_manager/show_books.html', {'books': books, 'form': form})
+    return render(request, 'book_manager/list_books.html', {'books': books, 'form': form})
+
+
+def show_author(request, author_id):
+    try:
+        author = Author.objects.get(pk=author_id)
+    except Author.DoesNotExist:
+        messages.error(request, 'There is no author with this id!')
+        return redirect(list_authors)
+    return render(request, 'book_manager/show_author.html', {'author': author})
