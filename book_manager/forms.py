c5868d5165515b86f07f6090d93a95cc7be409d2
michael.schori@talent-factory.ch <michael.schori@talent-factory.ch>
2020-08-29 12:03:24 +0200

    Added update for authors

--- book_manager/forms.py
+++ book_manager/forms.py
@@ -12,3 +12,15 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.is_update = kwargs.pop('is_update')
+        super(BookForm, self).__init__(*args, **kwargs)
+
+    def clean_isbn(self):
+        if self.is_update:
+            return self.cleaned_data['isbn']
+        else:
+            if Book.objects.filter(pk=self.cleaned_data['isbn']).exists():
+                raise forms.ValidationError('This isbn is alread in use.')
--- book_manager/views.py
+++ book_manager/views.py
@@ -17,7 +17,7 @@ def list_authors(request):
 
 def list_books(request):
     books = Book.objects.all()
-    form = BookForm()
+    form = BookForm(is_update=False)
     return render(request, 'book_manager/list_books.html', {'books': books, 'form': form})
 
 
@@ -27,7 +27,18 @@ def show_author(request, author_id):
     except Author.DoesNotExist:
         messages.error(request, 'There is no author with this id!')
         return redirect(list_authors)
-    return render(request, 'book_manager/show_author.html', {'author': author})
+    form = AuthorForm(initial={
+        'first_name': author.first_name,
+        'last_name': author.last_name,
+    })
+    if request.method == 'POST':
+        form = AuthorForm(request.POST, request.FILES)
+        if form.is_valid():
+            author.first_name = form.cleaned_data['first_name']
+            author.last_name = form.cleaned_data['last_name']
+            author.image = form.cleaned_data['image']
+            author.save()
+    return render(request, 'book_manager/show_author.html', {'author': author, 'form': form})
 
 
 def show_book(request, book_id):
@@ -36,4 +47,18 @@ def show_book(request, book_id):
     except Book.DoesNotExist:
         messages.error(request, 'There is no book with this id!')
         return redirect(list_books)
-    return render(request, 'book_manager/show_book.html', {'book': book})
+    form = BookForm(is_update=True, initial={
+        'isbn': book.isbn,
+        'title': book.title,
+        'read': book.read,
+        'authors': [i.id for i in book.authors.all()]
+    })
+    if request.method == 'POST':
+        form = BookForm(request.POST, is_update=True)
+        if form.is_valid():
+            book.title = form.cleaned_data['title']
+            print(book.title)
+            # for author in form.cleaned_data['authors']:
+            #     book.authors.add(Author.objects.get(pk=author.id))
+            messages.success(request, 'Book successfully updated.')
+    return render(request, 'book_manager/show_book.html', {'book': book, 'form': form})
