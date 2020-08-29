57180a90b2828f199798720cf0fc6aff0bbba498
michael.schori@talent-factory.ch <michael.schori@talent-factory.ch>
2020-08-29 08:55:43 +0200

    new changes

--- book_manager/views.py
+++ book_manager/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from .models import Author, Book
+from .forms import AuthorForm, BookForm
+from django.contrib import messages
 
 
 def show_authors(request):
     authors = Author.objects.all()
-    return render(request, 'book_manager/show_authors.html', {'authors': authors})
+    form = AuthorForm()
+    if request.method == 'POST':
+        form = AuthorForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Author successfully created.')
+    return render(request, 'book_manager/show_authors.html', {'authors': authors, 'form': form})
 
 
 def show_books(request):
     books = Book.objects.all()
-    return render(request, 'book_manager/show_books.html', {'books': books})
+    form = BookForm()
+    return render(request, 'book_manager/show_books.html', {'books': books, 'form': form})
